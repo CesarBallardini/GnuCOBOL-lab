2a519145d1d9606c7025a0307838b8b1522383ca
Cesar Ballardini <cesar.ballardini@gmail.com>
2021-05-16 18:34:08 -0300

    agregada ayuda para correr NotePad++

--- provision/instala-notepad-plus-plus.sh
+++ provision/instala-notepad-plus-plus.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 sudo snap install notepad-plus-plus
 
+echo "Para que funcione Notepad++ se debe correr con el siguiente mandato:"
+echo "echo 'XAUTHORITY=$HOME/.Xauthority /snap/bin/notepad-plus-plus'
+echo
+
